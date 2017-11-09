9d2579021b0f56817d23db95515958406cff7e16
Rany Keddo <purzelrakete@gmail.com>
2017-11-09 21:01:57 +0100

    Train on all possible images

--- nnjl/deep.jl
+++ nnjl/deep.jl
@@ -1,24 +1,26 @@
+# Given 2x2 images, where each pixel can take a value of 0 or 1, detect
+# diagonals, eg:
+#
+#   10
+#   01
+
 # functions
 sigmoid(x) = 1 ./ (1 + exp.(-x))
 relu(x) = max.(0, x)
 predict(x, A, B) = sigmoid(B * relu(A * x))
 
 # training data
-X = [
-  1 0 1 1
-  0 1 0 1
-  0 1 0 0
-  1 0 0 0
-]
-
-y = [1 1 0 0]
+n = 4
+m = 2^n
+X = [parse(Int, bin(x, 4)[y]) for y = 1:n, x = 0:m-1] # every 2x2 mono image
+y = zeros(Int, 1, m); y[1, 6+1] = 1; y[1, 9+1] = 1
 
 # parameter search
 A = zeros(3, 4)
 B = zeros(1, 3)
 best_likelihood = 0.0
 
-for i = 0:10_000
+for i = 0:100_000
   A_next = A + (0.5 - rand(3, 4)) * 0.1
   B_next = B + (0.5 - rand(1, 3)) * 0.1
 
